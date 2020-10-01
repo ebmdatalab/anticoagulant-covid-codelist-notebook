7bff5c42ac2b0ef3fca67a5c3058101a24cc7ad8
HelenCEBM <helencebm@gmail.com>
2020-10-01 11:34:22 +0100

    Adding chemical name

--- notebooks/diffable_python/anticoagulants.py
+++ notebooks/diffable_python/anticoagulants.py
@@ -31,22 +31,31 @@ import os
 import pandas as pd
 
 # ## All anticoagulants preparations  <a id='all'></a>
+#
+# Jump to:
+# - All anticoagulant preparations (this section)
+# - [Parenteral anticoagulants](#inj)
+# - [Oral anticoagulants](#oac)
+#    - [Warfarin](#warf) 
+#    - [DOACs](#doac) 
 
 # +
 sql = '''WITH bnf_codes AS (
-  SELECT bnf_code FROM hscic.presentation WHERE 
-bnf_code LIKE '0208%'  #BNF section anticoagulants
+  SELECT bnf_code, chemical FROM hscic.presentation p
+  LEFT JOIN (SELECT DISTINCT chemical, chemical_code FROM `ebmdatalab.hscic.bnf`) bnf ON SUBSTR(p.bnf_code,1,9)=bnf.chemical_code
+  WHERE 
+  bnf_code LIKE '0208%'  #BNF section anticoagulants
 )
 
-SELECT "vmp" AS type, id, bnf_code, nm
-FROM dmd.vmp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
+SELECT "vmp" AS type, id, v.bnf_code, nm, b.chemical
+FROM dmd.vmp v
+INNER JOIN bnf_codes b ON v.bnf_code=b.bnf_code
 
 UNION ALL
 
-SELECT "amp" AS type, id, bnf_code, descr
-FROM dmd.amp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
+SELECT "amp" AS type, id, a.bnf_code, descr, b.chemical
+FROM dmd.amp a
+INNER JOIN bnf_codes b ON a.bnf_code=b.bnf_code
 
 ORDER BY type, nm, bnf_code, id'''
 
@@ -57,45 +66,40 @@ complete_anticoagulant_codelist
 # -
 
 # ## Parenteral  <a id='inj'></a>
+#
+# Jump to:
+# - [All anticoagulant preparations](#all)
+# - Parenteral anticoagulants (this section)
+# - [Oral anticoagulants](#oac)
+#    - [Warfarin](#warf) 
+#    - [DOACs](#doac) 
 
-# +
-sql = '''WITH bnf_codes AS (
-  SELECT bnf_code FROM hscic.presentation WHERE 
-bnf_code LIKE '0208010%'  #BNF section anticoagulants - parenteral
-)
-
-SELECT "vmp" AS type, id, bnf_code, nm
-FROM dmd.vmp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
-
-UNION ALL
-
-SELECT "amp" AS type, id, bnf_code, descr
-FROM dmd.amp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
-
-ORDER BY type, nm, bnf_code, id'''
-
-parenteral_anticoagulant_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','parenteral_anticoagulant_codelist.csv'))
+parenteral_anticoagulant_codelist = complete_anticoagulant_codelist.loc[complete_anticoagulant_codelist["bnf_code"].str[:7]=="0208010"]
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
-parenteral_anticoagulant_codelist
-# -
+parenteral_anticoagulant_codelist.reset_index()
 
 # ## Oral Anticoagulants  <a id='oac'></a>
 #
-#
+# Jump to:
+# - [All anticoagulant preparations](#all)
+# - [Parenteral anticoagulants](#inj)
+# - Oral anticoagulants (this section)
+#    - [Warfarin](#warf) 
+#    - [DOACs](#doac) 
 
 # +
 sql = '''WITH bnf_codes AS (
-  SELECT bnf_code FROM hscic.presentation WHERE 
-bnf_code LIKE '0208020%'  #BNF section anticoagulants - oral
+  SELECT bnf_code, chemical FROM hscic.presentation p
+  LEFT JOIN (SELECT DISTINCT chemical, chemical_code FROM `ebmdatalab.hscic.bnf`) bnf ON SUBSTR(p.bnf_code,1,9)=bnf.chemical_code
+  WHERE 
+  bnf_code LIKE '0208020%'  #BNF section anticoagulants - oral
 )
 
-SELECT *
-FROM measures.dmd_objs_with_form_route
-WHERE bnf_code IN (SELECT * FROM bnf_codes) 
-AND 
+SELECT d.*, b.chemical
+FROM measures.dmd_objs_with_form_route d
+INNER JOIN bnf_codes b ON d.bnf_code=b.bnf_code
+WHERE
 obj_type IN ('vmp', 'amp')
 AND
 form_route LIKE '%.oral%' 
@@ -109,61 +113,36 @@ oral_anticoagulant_codelist
 # -
 
 # ## Warfarin <a id='warf'></a>
+#
+# Jump to:
+# - [All anticoagulant preparations](#all)
+# - [Parenteral anticoagulants](#inj)
+# - [Oral anticoagulants](#oac)
+#    - Warfarin (this section)
+#    - [DOACs](#doac) 
 
 # +
-sql = '''WITH bnf_codes AS (
-  SELECT bnf_code FROM hscic.presentation WHERE 
-bnf_code LIKE '0208020V0%'  #BNF section anticoagulants - warfarin
-)
 
-SELECT "vmp" AS type, id, bnf_code, nm
-FROM dmd.vmp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
+wafarin_codelist = complete_anticoagulant_codelist.loc[complete_anticoagulant_codelist["bnf_code"].str[:9]=="0208020V0"]
 
-UNION ALL
-
-SELECT "amp" AS type, id, bnf_code, descr
-FROM dmd.amp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
-
-ORDER BY type, nm, bnf_code, id'''
-
-wafarin_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','wafarin_codelist.csv'))
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
-wafarin_codelist
+wafarin_codelist.reset_index(drop=True)
 
 
 # -
 
 # ## DOACs  <a id='doac'></a>
+#
+# Jump to:
+# - [All anticoagulant preparations](#all)
+# - [Parenteral anticoagulants](#inj)
+# - [Oral anticoagulants](#oac)
+#    - [Warfarin](#warf) 
+#    - DOACs (this section)
 
 # +
-sql = '''WITH bnf_codes AS (
-  SELECT bnf_code FROM hscic.presentation WHERE 
-bnf_code LIKE '0208020Z0%'  OR  #BNF section anticoagulants - apixaban
-bnf_code LIKE '0208020X0%'  OR  #BNF section anticoagulants - dabigatran
-bnf_code LIKE '0208020AA%'  OR  #BNF section anticoagulants - edoxaban
-bnf_code LIKE '0208020Y0%'      #BNF section anticoagulants - rivarxaban
-
-)
-
-SELECT "vmp" AS type, id, bnf_code, nm
-FROM dmd.vmp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
-
-UNION ALL
-
-SELECT "amp" AS type, id, bnf_code, descr
-FROM dmd.amp
-WHERE bnf_code IN (SELECT * FROM bnf_codes)
-
-ORDER BY type, nm, bnf_code, id'''
-
-doac_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','doac_codelist.csv'))
+doac_codelist = complete_anticoagulant_codelist.loc[complete_anticoagulant_codelist["bnf_code"].str[:9].isin(["0208020Z0","0208020X0","0208020AA","0208020Y0"])]
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_colwidth', None)
-doac_codelist
-# +
-
-
+doac_codelist.reset_index(drop=True)
